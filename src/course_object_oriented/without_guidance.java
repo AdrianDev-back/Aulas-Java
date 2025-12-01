d9bfc911906a0c379ab1a4c655bc56027a4eb473
Gregory Adrian <gregoryadrian.dev@gmail.com>
2025-12-01 01:26:43 -0300

    update_class

--- src/course_object_oriented/without_guidance.java
+++ src/course_object_oriented/without_guidance.java
@@ -1,10 +1,43 @@
 package course_object_oriented;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 public class without_guidance {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault (Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		double xA, xB, xC, yA, yB, yC;
+		
+		System.out.println("Enter the measure of triangle x: ");
+		xA = sc.nextDouble();
+		xB = sc.nextDouble();
+		xC = sc.nextDouble();
+		System.out.println("Enter the measure of triangle y: ");
+		yA= sc.nextDouble();
+		yB = sc.nextDouble();
+		yC = sc.nextDouble();
+		
+		double p = (xA + xB + xC ) / 2.0;
+		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+		
+		p = (yA + yB + yC) / 2.0;
+		double areaY = Math.sqrt(p * (p - yA) * (p- yB) * (p- yC));
+		
+		System.out.printf("Triangle area: %.4f\n",areaX);
+		System.out.printf("Triangle area: %.4f\n",areaY);
+		
+		if (areaX > areaY) {
+			System.out.println("Larger area: X");
+		}
+		else {
+			System.out.println("Larger area: Y");
+		}
+				
+	    sc.close();
 	}
 
 }
