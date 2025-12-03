2b0df203895276f27443c4d17de4fac2b55af8f7
Gregory Adrian <gregoryadrian.dev@gmail.com>
2025-12-03 12:17:13 -0300

    updateClass

--- src/course_object_oriented/without_guidance.java
+++ src/course_object_oriented/without_guidance.java
@@ -1,31 +1,37 @@
 package course_object_oriented;
 
 import java.util.Scanner;
+
+import entities.Triangle;
+
 import java.util.Locale;
 
 public class without_guidance {
 
 	public static void main(String[] args) {
+		//Transformando um programa sem orientação para um programa com orientação a objeto
 		
 		Locale.setDefault (Locale.US);
 		Scanner sc = new Scanner (System.in);
 		
-		double xA, xB, xC, yA, yB, yC;
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
 		
 		System.out.println("Enter the measure of triangle x: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		System.out.println("Enter the measure of triangle y: ");
-		yA= sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		y.a= sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 		
-		double p = (xA + xB + xC ) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+		double p = (x.a + x.b + x.c ) / 2.0;
+		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 		
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p * (p - yA) * (p- yB) * (p- yC));
+		p = (y.a + y.b + y.c) / 2.0;
+		double areaY = Math.sqrt(p * (p - y.a) * (p- y.b) * (p- y.c));
 		
 		System.out.printf("Triangle area: %.4f\n",areaX);
 		System.out.printf("Triangle area: %.4f\n",areaY);
