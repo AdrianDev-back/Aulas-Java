4d113ee9b118915cf955bac7e1e81db0087148c0
Gregory Adrian <gregoryadrian.dev@gmail.com>
2026-01-05 00:59:26 -0300

    Concluded_MembersStatics

--- src/entities/Calculator.java
+++ src/entities/Calculator.java
@@ -2,14 +2,14 @@ package entities;
 
 public class Calculator {
 	
-	public static final double PI = 3.1459;
+	public static final double PI = 3.14159;
 	
 	public static double circumference(double radius) {
 		return 2.0 * PI * radius;
 	}
 	
 	public static double volume(double radius) {
-		return 4.0 * PI * radius * radius * radius/ 3.0;
+		return 4.0 * PI * radius * radius * radius / 3.0;
 	}
 
 }
